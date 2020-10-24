80bb55c9674a2dd4e939467e79e94afee8189a1d
Hunter Dermott <hbdermott@gmail.com>
2020-10-24 16:11:13 -0700

    Init/Added CRC64 class functionality

---
+++ crc.h
@@ -0,0 +1,10 @@
+#include <stdint.h>
+class CRC64 {
+    public:
+        CRC64();
+        ~CRC64() {delete [] table;}
+        uint64_t getCRC(uint8_t bytes[], int size);
+    private:
+        uint64_t * table = nullptr;
+        const uint64_t m_poly = 0x42F0E1EBA9EA3693;
+};
